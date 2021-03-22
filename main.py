54d4dd28d1c3b0c8fd9d949fdad49830de0b7766
Suyash L <suyashblal@gmail.com>
2021-03-22 21:44:08 +0000

    Added in the Number value fully done.

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ if (number % 2) == 0:
  print ("Number is... EVEN!")
 else:
   print("Number is... ODD!")
+
+
+
+
+  #New Code: Write a program that takes a number and tells the user if the
+#number is greater than 10, less than 10, or equal to 10. Don't
+#forget to convert the string into an integer.
+
+number2 = int(input("Enter Number another number:"))
+if (number2)> 10:
+    print("Number is greater than... 10!")
+elif number2< 10:
+    print("Number is less than 10!")
+else: 
+    print("Number is 10!")
