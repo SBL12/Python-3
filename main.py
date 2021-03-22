5682ff95a87386852b1047f42ec547e3082841d7
Suyash L <suyashblal@gmail.com>
2021-03-22 21:25:35 +0000

    Python 3 lab. Saving.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #Asking a number from the user: Even Or odd
 
+
+
 number = int(input("Enter Number to pass."))
-
-
 #checking number E or O
 if (number % 2) == 0:
-    print ("Number is + number + ... EVEN!")
+ print ("Number is... EVEN!")
 else:
-    print("Number is + number + ... ODD!")
+  print("Number is... ODD!")
